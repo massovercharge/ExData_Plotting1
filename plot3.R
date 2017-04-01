d008545e85cbe4bbee1b88a47d5db10182e103c9
massovercharge <danielweltzpedersen@Daniels-MacBook-Pro.local>
2017-04-02 00:36:25 +0200

    Added some comments to help understanding

--- plot3.R
+++ plot3.R
@@ -13,7 +13,7 @@ data <- cbind("DateTime" = strptime(paste(data$Date, data$Time, sep=" "), "%d/%m
 # Write plot to PNG graphics device
 png(filename = "plot3.png", width = 480, height = 480, units = "px", bg = "transparent")
 
-plot(data$DateTime, data$Sub_metering_1, type="l", ylab="Energy Submetering", xlab="")
+plot(data$DateTime, data$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
 lines(data$DateTime, data$Sub_metering_2, type="l", col="red")
 lines(data$DateTime, data$Sub_metering_3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, col=c("black", "red", "blue"))
